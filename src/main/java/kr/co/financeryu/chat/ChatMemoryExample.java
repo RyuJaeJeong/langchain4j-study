e4c561a73567307e337ac485b9d73d18b8e221e6
DGB\3240133 <fb0982@naver.com>
2025-12-23 14:52:40 +0900

    feat: Chat memory 테스트 코드 수정 등 2025-12-23 류재정 프로

--- src/main/java/kr/co/financeryu/chat/ChatMemoryExample.java
+++ src/main/java/kr/co/financeryu/chat/ChatMemoryExample.java
@@ -3,8 +3,10 @@ package kr.co.financeryu.chat;
 import dev.langchain4j.chain.ConversationalChain;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.memory.chat.TokenWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiTokenCountEstimator;
 import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
 import io.github.cdimascio.dotenv.Dotenv;
 
@@ -16,7 +18,6 @@ public class ChatMemoryExample {
 
     // Cons
     public ChatMemoryExample() {
-        this.memoryStore = new InMemoryChatMemoryStore();
         Dotenv dotenv = Dotenv.load();
         String OPENAI_MODEL_NM = dotenv.get("OPENAI_MODEL_NM");
         String OPENAI_API_KEY = dotenv.get("OPENAI_API_KEY");
@@ -24,18 +25,24 @@ public class ChatMemoryExample {
                 .modelName(OPENAI_MODEL_NM)
                 .apiKey(OPENAI_API_KEY)
                 .build();
+        this.memoryStore = new InMemoryChatMemoryStore();
+    }
+
+    public ChatMemoryExample(ChatModel model) {
+        this.model = model;
+        this.memoryStore = new InMemoryChatMemoryStore();
     }
 
     // Method
     public String chat(String threadId, String query){
-        ChatMemory memory = MessageWindowChatMemory.builder()
+        ChatMemory chatMemory = TokenWindowChatMemory.builder()
                 .id(threadId)
-                .maxMessages(10)
+                .maxTokens(13107, new OpenAiTokenCountEstimator("gpt-5-mini"))
                 .chatMemoryStore(this.memoryStore)
                 .build();
         ConversationalChain chain = ConversationalChain.builder()
                 .chatModel(this.model)
-                .chatMemory(memory)
+                .chatMemory(chatMemory)
                 .build();
         return chain.execute(query);
     }
--- src/test/java/kr/co/financeryu/chat/ChatMemoryExampleTest.java
+++ src/test/java/kr/co/financeryu/chat/ChatMemoryExampleTest.java
@@ -2,25 +2,22 @@ package kr.co.financeryu.chat;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class ChatMemoryExampleIntergrationTest {
+class ChatMemoryExampleTest {
 
     @Test
     void chat() {
-        Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
         ChatMemoryExample chatMemoryExample = new ChatMemoryExample();
 
         // given
         String threadId = "test1";
-        String[] arr = {"내 이름은 류재정이야", "내 이름이 뭐라고?"};
+        String[] arr = {"내 이름은 류재정이야, ai 서비스 개발하고있어", "내가 뭐하는 사람이라고?", "오 대단한걸?"};
         for (String query: arr){
             // when
-            if(query.equals("END")) break;
             String answer = chatMemoryExample.chat(threadId, query);
+            System.out.println("query: " + query);
             System.out.println("answer: " + answer);
             sb.append(answer);
         }
