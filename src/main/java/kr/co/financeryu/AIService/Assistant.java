d5e3e30a30983c1e8b1ab51cd1ecfa595bb0cfcb
DGB\3240133 <fb0982@naver.com>
2025-12-24 17:19:56 +0900

    feat: AIService 테스트 1. 스트리밍 출력 2. memoryId 추가 3. memoryStore 추가 등 2025-12-24 류재정 프로

--- src/main/java/kr/co/financeryu/AIService/Assistant.java
+++ src/main/java/kr/co/financeryu/AIService/Assistant.java
@@ -1,6 +1,10 @@
 package kr.co.financeryu.AIService;
 
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.TokenStream;
+import dev.langchain4j.service.UserMessage;
+
 public interface Assistant {
     String chat(String userMessage);
-
+    TokenStream stream(@MemoryId String memoryId,  @UserMessage String message);
 }
