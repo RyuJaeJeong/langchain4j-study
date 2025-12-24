b49307e3745514b7cdcd28885a239331fd01e2f6
DGB\3240133 <fb0982@naver.com>
2025-12-24 11:51:42 +0900

    feat: AIService 테스트(시스템 프롬프트 입력) 2025-12-24 류재정 프로

--- src/main/java/kr/co/financeryu/AIService/Translator.java
+++ src/main/java/kr/co/financeryu/AIService/Translator.java
@@ -1,10 +1,11 @@
 package kr.co.financeryu.AIService;
 
+import dev.langchain4j.service.Result;
 import dev.langchain4j.service.SystemMessage;
 
 public interface Translator {
 
-    @SystemMessage("당신은 한국어 번역기입니다, 사용자의 입력을 한국어로 번역하세요")
-    String chat(String userMessage);
+//    @SystemMessage("당신은 한국어 번역기입니다, 사용자의 입력을 한국어로 번역하세요")
+    Result<String> chat(String userMessage);
 
 }
