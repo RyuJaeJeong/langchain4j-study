db7470fe8e57ea4c2c0960dfa3438285dfd598c8
DGB\3240133 <fb0982@naver.com>
2025-12-26 11:46:16 +0900

    fix: 불 필요한 라인 제거 2025-12-26 류재정 프로

--- src/test/java/kr/co/financeryu/chat/ChatStreamingModelExampleTest.java
+++ src/test/java/kr/co/financeryu/chat/ChatStreamingModelExampleTest.java
@@ -1,12 +1,10 @@
 package kr.co.financeryu.chat;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChatStreamingModelExampleTest {
 
-    @Disabled("Manual streaming test")
     @Test
     void chat(){
         ChatStreamingModelExample stream = new ChatStreamingModelExample();
